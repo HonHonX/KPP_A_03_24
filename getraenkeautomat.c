4b01fb2cdaf028931ce34489fee7d43fe7dc273a
Sherry <119943082+HonHonX@users.noreply.github.com>
2022-12-13 16:43:57 +0100

    Update getraenkeautomat.c

--- getraenkeautomat.c
+++ getraenkeautomat.c
@@ -44,11 +44,10 @@ int main () {
 		while (restbetrag > 0 && restbetrag<=warenwert) {
 			printf("\nEs fehlen noch %.2f Euro.\nBitte werfen Sie ein Geldstueck ein oder drücken Sie die 0 um den Vorgang abzubrechen: ",warenwert);
 			scanf("%f",&geldstueck);
-			if (geldstueck==0) {
+			if (geldstueck==0) 
 				break;
-			} else {
+			else 
 				restbetrag-=geldstueck;
-			}
 		}
 		
 		if (anzahl!=0) {
@@ -56,18 +55,18 @@ int main () {
 			for (int i=1; i<=anzahl; i++) {
 				printf("\nFlasche %d von %d wurde ausgegeben", i, anzahl);
 			}
-		}
+			//Abgleich der Zahlung
+			if (restbetrag==0) 
+				printf("\nVielen Dank. Bitte entnehmen Sie Ihr(e) Getraenk(e).");
+			else 
+				printf("\n\nSie haben zu viel eingeworfen. Bitte entnehmen Sie ggf. Ihr(e) Getränk(e) und den Restbetrag von %.2f€", restbetrag*(-1));
+		} else 
+				printf("Vielleicht ein anderes Mal!");
 		
-		//Abgleich der Zahlung
-		if (restbetrag==0) {
-			printf("\nVielen Dank. Bitte entnehmen Sie Ihr(e) Getraenk(e).");
-		} else {
-			printf("\n\nSie haben zu viel eingeworfen. Bitte entnehmen Sie ggf. Ihr(e) Getränk(e) und den Restbetrag von %.2f€", restbetrag*(-1));
-		}
+		
 
-	} else {
+	} else 
 		printf("\nIhre Wahl war leider ungültig.Vorgang abgebrochen.");
-	}
 
 	//Ende
 	printf("\n\n");
