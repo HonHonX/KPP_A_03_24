26e2c482962a9543c1eda8c84b998a6ebc7ad255
Sherry <119943082+HonHonX@users.noreply.github.com>
2022-12-13 16:55:50 +0100

    Update getraenkeautomat.c

--- getraenkeautomat.c
+++ getraenkeautomat.c
@@ -7,7 +7,7 @@ int main () {
 	*/
 
 	//Variablen
-	int wahlgetraenk,anzahl;
+	int wahlgetraenk,anzahl=0;
 	float warenwert,geldstueck,restbetrag; 
 
 	printf("\n ************************************* ");
@@ -24,11 +24,9 @@ int main () {
  	printf("\n[3] Bier (2.00€)");	
 	printf("\n\nBitte geben Sie 1,2 oder 3 für ihr gewünschtes Getränk ein: ");
 	scanf("%d",&wahlgetraenk);
-	printf("\n\nBitte geben Sie die gewünschte Menge ein: ");
-	scanf("%d",&anzahl);
-
+	
 	//Bezahlvorgang
-	if (wahlgetraenk>=1 && wahlgetraenk<=3 && anzahl!=0) {
+	if (wahlgetraenk>=1 && wahlgetraenk<=3) {
 		switch (wahlgetraenk) {
 			case 1: warenwert=anzahl*0.50;
 			break;
@@ -36,34 +34,41 @@ int main () {
 			break;
 			case 3: warenwert=anzahl*2.00;
 			break;
-		}
+		}	
 		
-		printf("\n\n--- Bezahlvorgang ---");
+		//Eine Mengenauswahl erfolgt nur dann, wenn ein gültiges Getränk ausgewählt wurde
+		printf("\n\nBitte geben Sie die gewünschte Menge ein: ");
+		scanf("%d",&anzahl);
+		
+		//Bei der Menge 0 passiert nichts weiter, sonst wird zur Zahlung aufgefordert
+		if (anzahl!=0) {
+			
+			printf("\n\n--- Bezahlvorgang ---");
 
-		//Aufforderung zum Geldeinwurf
-		restbetrag = warenwert;
-		while (restbetrag > 0 && restbetrag<=warenwert) {
-			printf("\nEs fehlen noch %.2f Euro.\nBitte werfen Sie ein Geldstueck ein oder drücken Sie die 0 um den Vorgang abzubrechen: ",warenwert);
-			scanf("%f",&geldstueck);
-			if (geldstueck==0) 
-				break;
+			restbetrag = warenwert;
+			while (restbetrag > 0 && restbetrag<=warenwert) {
+				printf("\nEs fehlen noch %.2f Euro.\nBitte werfen Sie ein Geldstueck ein oder drücken Sie die 0 um den Vorgang abzubrechen: ",warenwert);
+				scanf("%f",&geldstueck);
+				if (geldstueck==0) 
+					break;
+				else 
+					restbetrag-=geldstueck;
+			}	
+		
+			//Getränkeausgabe
+			printf("\n\n--- Getränkeausgabe ---");
+			for (int i=1; i<=anzahl; i++) 
+				printf("\nFlasche %d von %d wurde ausgegeben", i, anzahl);
+		
+			//Abgleich der Zahlung
+			if (restbetrag==0) 
+				printf("\nVielen Dank. Bitte entnehmen Sie Ihr(e) Getraenk(e).");
 			else 
-				restbetrag-=geldstueck;
+				printf("\n\nSie haben zu viel eingeworfen. Bitte entnehmen Sie ggf. Ihr(e) Getränk(e) und den Restbetrag von %.2f€", restbetrag*(-1));
 		}
-		
-		//Getränkeausgabe
-		printf("\n\n--- Getränkeausgabe ---");
-		for (int i=1; i<=anzahl; i++) 
-			printf("\nFlasche %d von %d wurde ausgegeben", i, anzahl);
-		
-		//Abgleich der Zahlung
-		if (restbetrag==0) 
-			printf("\nVielen Dank. Bitte entnehmen Sie Ihr(e) Getraenk(e).");
-		else 
-			printf("\n\nSie haben zu viel eingeworfen. Bitte entnehmen Sie ggf. Ihr(e) Getränk(e) und den Restbetrag von %.2f€", restbetrag*(-1));
 			
 	} else 
-		printf("\nIhre Wahl war leider ungültig.Vorgang abgebrochen.");
+		printf("\nIhre Wahl war leider ungültig.Vorgang abgebrochen.Bis bald");
 
 	//Ende
 	printf("\n\n");
